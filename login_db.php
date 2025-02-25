8b38e147da06632a7aef677d2003cf26df5cda3b
Bareera Qureishi <bareera.qureishi7@gmail.com>
2025-02-25 22:02:36 +0530

    modified:   __pycache__/app.cpython-312.pyc 	modified:   app.py 	modified:   login_db.php 	modified:   static/css/login.css 	renamed:    "static/images/MacBook Wallpaper_Notebook_ Computador Papel de Parede _Fondo de pantalla de escritorio y port\303\241til.jpeg" -> static/images/bg_login.jpeg

--- login_db.php
+++ login_db.php
@@ -1,9 +1,15 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "login_db");
+$host = "localhost"; // or your MySQL server's IP
+$user = "root"; // MySQL username
+$pass = "Bareera@21"; // MySQL password
+$dbname = "heart_disease_db"; // Your database name
 
-// Checking connection
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
+// Create connection
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
 ?>
